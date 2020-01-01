b5d9e1968e6998857f4760cbf468e52e049c96c1
Douglas Naphas <douglasnaphas@gmail.com>
2020-01-01 16:18:54 -0500

    Attempt to use autocomplete with puppeteer
    https://stackoverflow.com/questions/53993443/using-autocomplete-in-puppeteer

--- puppeteer-autocomplete.js
+++ puppeteer-autocomplete.js
@@ -1,10 +1,34 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
+  const clickOptions = { delay: 200 };
+  const typeOptions = { delay: 10 };
+  const browserOptions = {};
+  browserOptions.slowMo = 200;
+  const timeoutMs = 2000;
+  const waitForNavigationOptions = { timeout: timeoutMs };
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(
-    "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_form_autocomplete"
-  );
+  await page.goto("http://localhost:8080/");
+  await page.waitForSelector('input[name="fname"]');
+  await page.click('input[name="fname"]');
+  await page.type('input[name="fname"]', "myName");
+  await page.waitForSelector('input[type="submit"]');
+  await page.waitFor(5000);
+  await page.click('input[type="submit"]');
+  await page.waitFor(5000);
+  await page.waitForSelector('input[name="fname"]');
+  await page.click('input[name="fname"]', clickOptions);
+  await page.waitFor(2000);
+  // await page.click('input[name="fname"]');
+  await page.waitFor(2000);
+  await page.keyboard.down("ArrowDown");
+  await page.waitFor(200);
+  await page.keyboard.up("ArrowDown");
+  await page.waitFor(2000);
+  // await page.keyboard.press("ArrowDown", { delay: 250 });
+  await page.waitFor(2000);
+  // await page.keyboard.press("ArrowRight", { delay: 250 });
+  await page.waitFor(5000);
   await browser.close();
 })();
